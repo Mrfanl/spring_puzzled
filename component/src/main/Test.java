743d8154a3ac1388b0a448351ce373a58331e4c1
Mrfanl <fanliang5959@sina.com>
2018-10-15 23:09:40 +0800

    在原先使用bean componment等注释下增加AnnotionConfigApplicationContext的使用

--- component/src/main/Test.java
+++ component/src/main/Test.java
@@ -1,12 +1,16 @@
 package main;
 
 import beans.Singer;
+import config.Config;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 public class Test {
     public static void main(String[] args){
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("component.xml");
+//        ApplicationContext ctx = new ClassPathXmlApplicationContext("component.xml");
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
       Singer xiaoming = ctx.getBean("xiaoming",Singer.class);
 //        Singer xiaoming2 = ctx.getBean("xiaoming",Singer.class);
 //
